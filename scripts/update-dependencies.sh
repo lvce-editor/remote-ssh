4a0820399101cca987e7b4680bc450019339c57d
Le Vivilet <levivilet@pm.me>
2026-08-05 11:55:12 +0000

    feature: update dependencies

---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+cd ..
+
+command_exists(){
+  command -v "$1" &> /dev/null
+}
+
+if ! command_exists "ncu"; then
+    echo "npm-check-updates is not installed"
+    npm i -g npm-check-updates
+else
+    echo "ncu is installed"
+fi
+
+function updateDependencies {
+  echo "updating dependencies..."
+  ncu -u -x @types/node -x @babel/preset-typescript -x typescript
+}
+
+                                                       updateDependencies             &&
+cd packages/build                                   && updateDependencies && cd ../.. &&
+cd packages/e2e                                     && updateDependencies && cd ../.. &&
+cd packages/extension                              && updateDependencies && cd ../.. &&
+cd packages/node                                  && updateDependencies && cd ../.. &&
+npm install                                         &&
+
+echo "Great Success!"
+
+sleep 2
