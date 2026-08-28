0aac2103fb351c193b99d023dec1846e65ac9acd
Le Vivilet <levivilet@pm.me>
2026-08-28 00:48:52 +0000

    Revert "test: isolate remote cli listener startup"
    This reverts commit 6ea1de5a9223d6d658473f0f18e9811592496de5.

--- packages/extension/src/parts/Connect/Connect.ts
+++ packages/extension/src/parts/Connect/Connect.ts
@@ -76,6 +76,10 @@ export const setRemoteWorkspaceUri = async (
   const connection = supportsConnectionCommand
     ? {
         command: WorkspaceConnection.commandId,
+        remoteCliUrl: WorkspaceConnection.getWebSocketUrlForBackend(
+          backend,
+          'shared-process',
+        ),
         workspacePath: backend.workspacePath,
       }
     : backend
